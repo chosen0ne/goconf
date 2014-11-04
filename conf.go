2df8b1853e7e9ab2726beb07b0379fbd11bc6d17
chosen0ne <louzhenlin86@126.com>
2014-11-04 18:54:19 +0800

    first ci

---
+++ conf.go
@@ -0,0 +1,339 @@
+/**
+ *
+ * @author  chosen0ne(louzhenlin86@126.com)
+ * @date    2014/11/03 15:03:49
+ */
+
+package conf
+
+import (
+    "os"
+    "bufio"
+    "strings"
+    "io"
+    "errors"
+    "strconv"
+)
+
+
+const (
+    _KV_SEP = ':'
+    _NEWLINE = '\n'
+    _SPACE_CHARS = " \t\n"
+    _ARRAY_TAG = '@'
+    _MAP_TAG = '$'
+    _DEFAULT_SEP = ' '
+    _COMPOSITE_LEFT = '['
+    _COMPOSITE_RIGHT = ']'
+
+    // Parse state
+    _PARSE_ERR = 0
+    _PARSE_START = 1
+    _PARSE_FOUND_KEY = 2
+    _PARSE_FOUND_VAL = 3
+    _PARSE_FOUND_KV = 4
+    _PARSE_DONE = 5
+)
+
+type ParseState int
+
+
+// ------- Item ------- //
+type Item struct {
+    rawKey  string
+    val     string
+    key     string
+}
+
+func (item *Item) Key() string {
+    return item.key
+}
+
+func (item *Item) ToInt() (int64, error) {
+    return strconv.ParseInt(item.val, 10, 64)
+}
+
+func (item *Item) ToString() string {
+    return item.val
+}
+
+func (item *Item) ToFloat() (float64, error) {
+    return strconv.ParseFloat(item.val, 64)
+}
+
+func (item *Item) ToIntArray() ([]int64, error) {
+    eleStr, err := item.ToStringArray()
+    if err != nil {
+        return nil, err
+    }
+
+    values := make([]int64, len(eleStr))
+    for idx, ele := range eleStr {
+        ele = strings.Trim(ele, _SPACE_CHARS)
+        val, err := strconv.ParseInt(ele, 10, 64)
+        if err != nil {
+            return nil, err
+        }
+        values[idx] = val
+    }
+
+    return values, nil
+}
+
+func (item *Item) ToFloatArray() ([]float64, error) {
+    eleStr, err := item.ToStringArray()
+    if err != nil {
+        return nil, err
+    }
+
+    values := make([]float64, len(eleStr))
+    for idx, ele := range eleStr {
+        ele = strings.Trim(ele, _SPACE_CHARS)
+        val, err := strconv.ParseFloat(ele, 64)
+        if err != nil {
+            return nil, err
+        }
+        values[idx] = val
+    }
+
+    return values, nil
+}
+
+func (item *Item) ToStringArray() ([]string, error) {
+    // Make sure the item is a Array
+    // Key: [$ArrayKey$;ArraySep] or [$ArrayKey]
+    keyLen := len(item.rawKey)
+    if item.rawKey[0] != _COMPOSITE_LEFT || item.rawKey[keyLen - 1] != _COMPOSITE_RIGHT ||
+            item.rawKey[1] != _ARRAY_TAG || keyLen < 4 {
+        errMsg := strings.Join([]string{"item is not a Array, key:", item.rawKey}, " ")
+        return nil, errors.New(errMsg)
+    }
+
+    // Extract sep
+    var sep byte
+    sepIdx := strings.LastIndexAny(item.rawKey, string([]byte{_ARRAY_TAG}))
+    if sepIdx == -1 {
+        return nil, errors.New("not found Array Tag: @")
+    }
+
+    if sepIdx == 1 {
+        sep = _DEFAULT_SEP
+    } else if sepIdx != keyLen -3 {
+        return nil, errors.New("Array Sep can only set to one char")
+    } else {
+        sep = item.rawKey[sepIdx + 1]
+    }
+
+    return strings.Split(item.val, string(sep)), nil
+}
+
+
+// ------- Conf ------- //
+type Conf struct {
+    in          io.Reader
+    filePath    string
+    items       map[string]*Item
+    curState    ParseState
+}
+
+func New(filePath string) (*Conf, error) {
+    conf := &Conf{}
+    conf.filePath = filePath
+    conf.items = make(map[string]*Item)
+    conf.curState = _PARSE_START
+
+    if err := conf.openFile(); err != nil {
+        return nil, err
+    }
+
+    return conf, nil
+}
+
+func (conf *Conf) Parse() error {
+    buf := bufio.NewReader(conf.in)
+    var parseBuf []byte
+    var err error
+    var key string
+    var valStr string
+    for {
+        switch conf.curState {
+        case _PARSE_START:
+            // Find KV Separator
+            parseBuf, err = buf.ReadBytes(_KV_SEP);
+
+            // Read a empty line, only contains ' \t\n'
+            if err == io.EOF && isSpaceBuf(parseBuf) {
+                conf.curState = _PARSE_DONE
+                continue
+            }
+
+            if err != nil {
+                conf.curState = _PARSE_ERR
+                continue
+            }
+            conf.curState = _PARSE_FOUND_KEY
+
+        case _PARSE_FOUND_KEY:
+            // Load Key string
+            key = extraceString(parseBuf)
+
+            // Read Value string
+            parseBuf, err = buf.ReadBytes(_NEWLINE);
+
+            // Found a non-empty value
+            if (err == io.EOF && !isSpaceBuf(parseBuf)) ||
+                    err == nil {
+                conf.curState = _PARSE_FOUND_VAL
+            } else {
+                conf.curState = _PARSE_ERR
+            }
+
+        case _PARSE_FOUND_VAL:
+            // Load Value string
+            valStr = extraceString(parseBuf)
+            conf.curState = _PARSE_FOUND_KV
+
+        case _PARSE_FOUND_KV:
+            rawKey := key
+            key = parseKey(key)
+            conf.items[key] = &Item{rawKey, valStr, key}
+            conf.curState = _PARSE_START
+
+        case _PARSE_ERR:
+            return err
+
+        case _PARSE_DONE:
+            return nil
+
+        default:
+            return errors.New("not support parse state")
+        }
+    }
+}
+
+func (conf *Conf) GetItem(key string) (*Item, error) {
+    item, ok := conf.items[key]
+    if !ok {
+        return nil, errors.New("non-exist key")
+    }
+    return item, nil
+}
+
+func (conf *Conf) HasItem(key string) bool {
+    _, ok := conf.items[key]
+    return ok
+}
+
+func (conf *Conf) Items() []*Item {
+    items := make([]*Item, len(conf.items))
+    idx := 0
+    for _, v := range conf.items {
+        items[idx] = v
+        idx++
+    }
+
+    return items
+}
+
+func (conf *Conf) GetInt(key string) (int64, error) {
+    item, err := conf.GetItem(key)
+    if err != nil {
+        return -1, err
+    }
+
+    return item.ToInt()
+}
+
+func (conf *Conf) GetFloat(key string) (float64, error) {
+    item, err := conf.GetItem(key)
+    if err != nil {
+        return -1, err
+    }
+
+    return item.ToFloat()
+}
+
+func (conf *Conf) GetString(key string) (string, error) {
+    item, err := conf.GetItem(key)
+    if err != nil {
+        return "", err
+    }
+
+    return item.val, nil
+}
+
+func (conf *Conf) GetIntArray(key string) ([]int64, error) {
+    item, err := conf.GetItem(key)
+    if err != nil {
+        return nil, err
+    }
+
+    return item.ToIntArray()
+}
+
+func (conf *Conf) GetFloatArray(key string) ([]float64, error) {
+    item, err := conf.GetItem(key)
+    if err != nil {
+        return nil, err
+    }
+
+    return item.ToFloatArray()
+}
+
+func (conf *Conf) GetStringArray(key string) ([]string, error) {
+    item, err := conf.GetItem(key)
+    if err != nil {
+        return nil, err
+    }
+
+    return item.ToStringArray()
+}
+
+func (conf *Conf) Close() error {
+    f := conf.in.(*os.File)
+    if err := f.Close(); err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func (conf *Conf) openFile() error {
+    f, err := os.Open(conf.filePath)
+    conf.in = f
+    return err
+}
+
+func extraceString(buf []byte) string {
+    str := string(buf[:len(buf) - 1])
+    return strings.Trim(str, _SPACE_CHARS)
+}
+
+func isSpaceBuf(buf []byte) bool {
+    for _, c := range buf {
+        if c != ' ' && c != '\n' && c != '\t' {
+            return false
+        }
+    }
+
+    return true
+}
+
+func parseKey(key string) string {
+    if len(key) < 4 {
+        return key
+    }
+
+    if key[0] != _COMPOSITE_LEFT || key[len(key) - 1] != _COMPOSITE_RIGHT ||
+            (key[1] != _ARRAY_TAG && key[1] != _MAP_TAG) {
+        return key
+    }
+
+    c := key[len(key) - 3]
+    if len(key) != 4 && (c == _ARRAY_TAG || c == _MAP_TAG) {
+        return key[2:len(key) - 3]
+    }
+
+    return key[2:len(key) - 1]
+}
+
---
+++ conf_test.go
@@ -0,0 +1,308 @@
+/**
+ *
+ * @author  chosen0ne(louzhenlin86@126.com)
+ * @date    2014/11/04 15:06:47
+ */
+
+package conf
+
+import (
+    "testing"
+    "reflect"
+    "chosen0ne.com/utils"
+    "bytes"
+)
+
+// ------- Tests for Item ------- //
+func TestItemSingleValOk(t *testing.T) {
+    var num int64 = 100
+    expected    := []interface{}    {num,     1.101,     "astring"}
+    input       := []string         {"100",   "1.101",   "astring"}
+    callMethod  := []string         {"ToInt", "ToFloat", "ToString"}
+    returnCount := []int            {2,       2,         1}
+
+    for i, _ := range input {
+        item := &Item{val: input[i]}
+        value := reflect.ValueOf(item)
+        method := value.MethodByName(callMethod[i])
+        results := method.Call(make([]reflect.Value, 0))
+
+        if len(results) != returnCount[i] {
+            t.Errorf("count of return args of '%s' error", callMethod[i])
+        }
+
+        // check error
+        if returnCount[i] > 1 {
+            err := results[1].Interface()
+            if err != nil && err.(error) != nil {
+                t.Errorf("failed to test '%s', err: %s", callMethod[i], err)
+            }
+        }
+
+        // check return value
+        expectVal := expected[i]
+        outputVal := results[0].Interface()
+        if outputVal != expectVal {
+            t.Error("output is not expected, output:", outputVal, ", expected:", expectVal)
+        }
+    }
+}
+
+func TestItemIntErr(t *testing.T) {
+    item := &Item{val: "not a int: 1"}
+    val, err := item.ToInt()
+    if err == nil || val != 0 {
+        t.Errorf("should return a err, val: %d, err: %s", val, err)
+    }
+}
+
+func TestItemFloatErr(t *testing.T) {
+    item := &Item{val: "not a float: 1.011"}
+    val, err := item.ToFloat()
+    if err == nil || val != 0 {
+        t.Errorf("should return a err, val: %f, err: %s", val, err)
+    }
+}
+
+func matchStringArray(output, expected []string) error {
+    if len(output) != len(expected) {
+        return utils.NewErr("length of expected and output is different output: %d, expected: %d",
+                len(output), len(expected))
+    }
+
+    for idx, str := range output {
+        if str != expected[idx] {
+            return utils.NewErr("not expected output, output: %s, expected: %s", output, expected)
+        }
+    }
+
+    return nil
+}
+
+// Test for Array use default separator ' '
+func TestItemStringArrayOk1(t *testing.T) {
+    item := &Item{"[@key1]", "abc de fg h", ""}
+    expected := []string {"abc", "de", "fg", "h"}
+
+    strArray, err := item.ToStringArray()
+    if err != nil {
+        t.Fatalf("failed to StringArray, err: %s", err)
+    }
+
+    err = matchStringArray(strArray, expected)
+    if err != nil {
+        t.Errorf("not expected output, err: %s", err)
+    }
+}
+
+// Test for Array use customed separator ':'
+func TestItemStringArrayOk2(t *testing.T) {
+    item := &Item{"[@key2@:]", "abc:de:dff", ""}
+    expected := []string {"abc", "de", "dff"}
+
+    strArray, err := item.ToStringArray()
+    if err != nil {
+        t.Fatalf("failed to StringArray, err: %s", err)
+    }
+
+    err = matchStringArray(strArray, expected)
+    if err != nil {
+        t.Errorf("not expected output, err: %s", err)
+    }
+}
+
+func TestItemStringArrayErr1(t *testing.T) {
+    item := &Item{"NotArrayKey", "xxxjjj", ""}
+    val, err := item.ToStringArray()
+    if err == nil || val != nil {
+        t.Errorf("need a 'not a Array Item' error")
+    }
+}
+
+func TestItemStringArrayErr2(t *testing.T) {
+    item := &Item{"[@InvaliSep@###]", "xx", ""}
+    val, err := item.ToStringArray()
+    if err == nil || val != nil {
+        t.Errorf("need a 'Array sep can only set to one char' error")
+    }
+}
+
+func TestItemIntArrayOk(t *testing.T) {
+    item := &Item{"[@IntArray]", "12 23 44 55", ""}
+    expected := []int64 {12, 23, 44, 55}
+
+    intArray, err := item.ToIntArray()
+    if err != nil {
+        t.Fatalf("failed to IntArray, err: %s", err)
+    }
+
+    if len(intArray) != len(expected) {
+        t.Errorf("length of expected and output is different output: %d, expected: %d",
+                len(intArray), len(expected))
+    }
+
+    for idx, v := range intArray {
+        if v != expected[idx] {
+            t.Errorf("not expected output, output: %s, expected: %s", intArray, expected)
+        }
+    }
+}
+
+func TestItemFloatArrayOk(t *testing.T) {
+    item := &Item{"[@FloatArray@;]", "1.1; 1.2; 12.33", ""}
+    expected := []float64 {1.1, 1.2, 12.33}
+
+    floatArray, err := item.ToFloatArray()
+    if err != nil {
+        t.Fatalf("failed to FloatArray, err: %s", err)
+    }
+
+    if len(floatArray) != len(expected) {
+        t.Errorf("length of expected and output is different output: %d, expected: %d",
+                len(floatArray), len(expected))
+    }
+
+    for idx, v := range floatArray {
+        if v != floatArray[idx] {
+            t.Errorf("not expected output, output: %s, expected: %s", floatArray, expected)
+        }
+    }
+}
+
+
+// ------- Tests for Conf ------- //
+func genConf(s string) *Conf {
+    in := bytes.NewBufferString(s)
+    return &Conf{in: in, curState: _PARSE_START, items: make(map[string]*Item)}
+}
+
+func TestConfParseOk1(t *testing.T) {
+    conf := genConf("item1: value1\n\n\nitem2: value2")
+
+    if err := conf.Parse(); err != nil {
+        t.Errorf("failed to parse, err: %s", err)
+    }
+}
+
+func TestConfParseOk2(t *testing.T) {
+    conf := genConf("[@int@;]: a;b;c\n[@int]: 1 2 3")
+
+    if err := conf.Parse(); err != nil {
+        t.Errorf("failed to parse, err: %s", err)
+    }
+}
+
+// Partial Key, without value
+func TestConfParseErr1(t *testing.T) {
+    conf := genConf("item1: valu\nitem1jfak")
+
+    if err := conf.Parse(); err == nil {
+        t.Errorf("need a EOF error")
+    }
+}
+
+func TestConfParseErr2(t *testing.T) {
+    conf := genConf("item1:  ")
+
+    if err := conf.Parse(); err == nil {
+        t.Errorf("need a EOF error")
+    }
+}
+
+func TestConfParseKeyOk1(t *testing.T) {
+    k := "a"
+    expected := "a"
+
+    if output := parseKey(k); output != expected {
+        t.Errorf("output is not expected, output: %s, expected: %s",
+                output, expected)
+    }
+}
+
+func TestConfParseKeyOk2(t *testing.T) {
+    k := "[xxxx]"
+    expected := "[xxxx]"
+    if output := parseKey(k); output != expected {
+        t.Errorf("output is not expected, output: %s, expected: %s",
+                output, expected)
+    }
+}
+
+func TestConfParseKeyOk3(t *testing.T) {
+    k := "[$anfa$1]"
+    expected := "anfa"
+    if output := parseKey(k); output != expected {
+        t.Errorf("output is not expected, output: %s, expected: %s",
+                output, expected)
+    }
+}
+
+func TestConfParseKeyOk4(t *testing.T) {
+    k := "[$kkkk]"
+    expected := "kkkk"
+    if output := parseKey(k); output != expected {
+        t.Errorf("output is not expected, output: %s, expected: %s",
+                output, expected)
+    }
+}
+
+func TestConfItemsOk(t *testing.T) {
+    conf := genConf("a:b\nc:d\ne:f\ng:h")
+    expected := map[string]int {"a": 1, "c": 1, "e": 1, "g": 1}
+
+    if err := conf.Parse(); err != nil {
+        t.Errorf("failed to parse, err: %s", err)
+    }
+
+    for _, item := range conf.Items() {
+        if _, ok := expected[item.Key()]; !ok {
+            t.Errorf("extra key %s ", item.Key())
+        }
+    }
+
+    for k, _ := range expected {
+        if !conf.HasItem(k) {
+            t.Errorf("key %s non-exist, items: %s", k, conf.Items())
+        }
+    }
+}
+
+func TestAll(t *testing.T) {
+    config, err := New("conf_sample.conf")
+    if err != nil {
+        t.Error("failed to New Conf")
+        return
+    }
+
+    defer config.Close()
+
+    if err := config.Parse(); err != nil {
+        t.Error("failed to Parse, err:", err)
+    }
+
+    // iterate items
+    t.Log("items:")
+    for _, item := range config.Items() {
+        t.Log("\t", item.Key())
+    }
+
+    strItem, err := config.GetString("StringItem")
+    if err == nil {
+        t.Log("StringItem =>", strItem)
+    }
+
+    intItem, err := config.GetInt("IntItem")
+    if err == nil {
+        t.Log("IntItem =>", intItem)
+    }
+
+    intArray, err := config.GetIntArray("IntArray")
+    if err == nil {
+        t.Log("IntArray =>", intArray)
+    }
+
+    floatArray, err := config.GetFloatArray("FloatArray")
+    if err == nil {
+        t.Log("FloatArray =>", floatArray)
+    }
+}
