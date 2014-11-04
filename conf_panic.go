9c10c466c5f0e0336f8f3d9ae80a67bd63d9faf1
chosen0ne <louzhenlin86@126.com>
2014-11-05 00:25:33 +0800

    add panic mode of Conf

---
+++ conf_panic.go
@@ -0,0 +1,88 @@
+/**
+ * A panic-recover way to use Conf.
+ * All the methods suffixed by BPY(By panic way) may throw exception,
+ * so they need to recover.
+ *
+ * @author  chosen0ne(louzhenlin86@126.com)
+ * @date    2014/11/04 21:50:38
+ */
+
+package conf
+
+
+func NewOrPanic(filePath string) *Conf {
+    conf, err := New(filePath)
+    if err != nil {
+        panic(err)
+    }
+
+    return conf
+}
+
+func (conf *Conf) ParseOrPanic() {
+    if err := conf.Parse(); err != nil {
+        panic(err)
+    }
+}
+
+func (conf *Conf) GetItemOrPanic(key string) *Item {
+    item, err := conf.GetItem(key)
+    if err != nil {
+        panic(err)
+    }
+    return item
+}
+
+func (conf *Conf) ToInt(key string) int64 {
+    val, err := conf.GetInt(key)
+    if err != nil {
+        panic(err)
+    }
+    return val
+}
+
+func (conf *Conf) ToFloat(key string) float64 {
+    val, err := conf.GetFloat(key)
+    if err != nil {
+        panic(err)
+    }
+    return val
+}
+
+func (conf *Conf) ToString(key string) string {
+    val, err := conf.GetString(key)
+    if err != nil {
+        panic(err)
+    }
+    return val
+}
+
+func (conf *Conf) ToIntArray(key string) []int64 {
+    val, err := conf.GetIntArray(key)
+    if err != nil {
+        panic(err)
+    }
+    return val
+}
+
+func (conf *Conf) ToFloatArray(key string) []float64 {
+    val, err := conf.GetFloatArray(key)
+    if err != nil {
+        panic(err)
+    }
+    return val
+}
+
+func (conf *Conf) ToStringArray(key string) []string {
+    val, err := conf.GetStringArray(key)
+    if err != nil {
+        panic(err)
+    }
+    return val
+}
+
+func (conf *Conf) CloseOrPanic() {
+    if err := conf.Close(); err != nil {
+        panic(err)
+    }
+}
